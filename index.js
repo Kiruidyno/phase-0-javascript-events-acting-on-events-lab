0c89f41e4e7ad5ce119cd63a5b4400cabafb5926
Kiruidyno <kirui472@gmail.com>
2023-03-27 22:15:06 +0300

    Updated complete assignment

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 // Your code here
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
-  
-  const dodger = document.getElementById("dodger");
-  
-  function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  }
-
-  
-document.addEventListener("keydown", function(e){
-    if (e.key === "ArrowRight") {
         moveDodgerLeft();
     }
 });
 
+const dodger = document.getElementById("dodger");
 
-function moveDodgerRight(){
+function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
 
-    if (left > 0){
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
         dodger.style.left = `${left + 1}px`;
     }
 }
